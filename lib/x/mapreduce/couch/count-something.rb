cb78bca1863f92928c4baf69d21e8c3d459e8767
Wlodek Bzyl <matwb@ug.edu.pl>
2011-03-09 12:11:13 +0100

    Poprawiłem skrypt count-something.rb

--- lib/x/mapreduce/couch/count-something.rb
+++ lib/x/mapreduce/couch/count-something.rb
@@ -4,42 +4,42 @@
 require 'couchrest'
 require 'pp'
 
-couch = CouchRest.new("http://sigma.ug.edu.pl:4000")
+couch = CouchRest.new("http://sigma.ug.edu.pl:5984")
 db = couch.database('gutenberg')
 
 puts "Now that we've parsed all those books into CouchDB,"
 puts "the queries we can run are incredibly flexible."
 puts "\nThe simplest query we can run is the total word"
 puts "count for all words in all documents:"
-puts "this will take a few minutes the first time. if it times out,"
-puts "just rerun this script in a few few minutes."
-
+puts "this will take a few minutes the first time."
+puts "If it times out, just rerun this script in a few few minutes."
+puts ""
 pp db.view('app/wc')
 
 puts "\nWe can also narrow the query down to just one word across all documents."
-puts "Here is the count for 'CAPTAIN' in all books:"
+puts "Here is the count for 'YELP' in all books:"
+puts ""
 
-word = 'captain'
+word = 'yelp'
 params = {
-  :startkey => [word], 
-  :endkey => [word, {}]
+  :startkey => [word],
+  :endkey => [word, {}],
+  :group_level => 2
   }
 pp db.view('app/wc', params)
-
-puts "\n--------"
 
 puts "\nWe scope the query using startkey and endkey params"
 puts "to take advantage of CouchDB's collation ordering."
 puts "\nWe can also count words on a per-title basis."
-puts "Here is the count for 'CAPTAIN' in The Skull:"
+puts "Here is the count for 'YOUTH' in 'THE IDIOT':"
+puts ""
 
 params = {
-  :key => ['captain', 'the skull']
+  :key => ['youth', 'the idiot']
 }
 pp db.view('app/wc', params)
 
+puts ""
 puts "The url looks like this:"
-puts "\n\thttp://sigma.ug.edu.pl:4000/gutenberg/_design/app/_view/wc?key=[\"very\",\"the skull\"]"
-puts "\nLokalna baza?"
-puts "\n\thttp://localhost:4000/gutenberg/_design/app/_view/wc?key=[\"very\",\"the skull\"]"
+puts "\n\thttp://localhost:5984/gutenberg/_design/app/_view/wc?key=[\"very\",\"the skull\"]"
 puts "\nTry dropping that in your browser..."
