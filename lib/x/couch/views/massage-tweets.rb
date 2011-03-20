991078b7aa2bcc36678a8f7edb179087417b951b
Wlodek Bzyl <matwb@univ.gda.pl>
2011-03-21 00:46:25 +0100

    Mała modyfikacja skryptu: kod w komentarzach.

--- lib/x/couch/views/massage-tweets.rb
+++ lib/x/couch/views/massage-tweets.rb
@@ -10,16 +10,18 @@ require 'couchrest'
 require 'pp'
 
 db = CouchRest.database("http://127.0.0.1:5984/nosql")
+#db = CouchRest.database("http://sigma.ug.edu.pl:5984/nosql")
 pager = CouchRest::Pager.new(db)
 
 out = CouchRest.database!("http://127.0.0.1:5984/nosql-slimmed")
+#out = CouchRest.database!("http://sigma.ug.edu.pl:5984/nosql-slimmed")
 
 pager.all_docs do |slice|
   chunk = slice.map do |element|
     doc = db.get(element["id"])
 
     date = DateTime.parse(doc['created_at'])
-    created_on =[date.year, date.month, date.day, date.hour, date.minute, date.second]
+    created_on =[date.year, date.month, date.day, date.hour, date.min, date.sec]
     {
       "_id" => doc["_id"],
       "text" => doc["text"],
