07d04de960c6f26f74c1e7c88a4e50fcc632c031
Wlodek Bzyl <matwb@univ.gda.pl>
2012-05-17 10:34:34 +0200

    poprawki w skrypcie poliqarp-downcase.rb

--- lib/x/db/mongodb/poliqarp-downcase.rb
+++ lib/x/db/mongodb/poliqarp-downcase.rb
@@ -2,11 +2,25 @@ require 'string_case_pl' # https://github.com/apohllo/string_pl
 
 require 'mongo'
 
+# collection = Mongo::Connection.new("localhost", 27017).db("test").collection("students")
+
 collection = Mongo::Connection.new("localhost", 27017).db("poliqarp").collection("toks")
 
-# puts coll.count
+puts "Documents in collection: #{coll.count}"
 
-collection.find.each do |doc|
+# Cursors
+#
+# http://www.mongodb.org/display/DOCS/How+to+do+Snapshotted+Queries+in+the+Mongo+Database
+# http://api.mongodb.org/ruby/current/Mongo/Cursor.html
+
+cursor = Mongo::Cursor.new(collection, snapshot: true)
+
+# puts cursor.class
+# puts cursor.full_collection_name
+# puts cursor.snapshot
+
+# collection.find.each do |doc|
+cursor.each do |doc|
   # puts "#{doc.inspect}"
   doc["orth"].downcase!
   # puts "#{doc.inspect}"
