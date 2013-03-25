8b653574e25b46a01832603c30590913c9efb1a4
Wlodek Bzyl <matwb@univ.gda.pl>
2013-03-25 23:54:51 +0100

    skrypt import-json.rb

--- pp/GeoIP/import-json.rb
+++ pp/GeoIP/import-json.rb
@@ -9,10 +9,18 @@ end
 
 a = IO.readlines('data/zips.json').shift(4)
 
-IO.readlines('data/zips.json').each_slice(3000) do |a|
-  puts a.size
-end
+# IO.readlines('data/zips.json').each_slice(3000) do |a|
+#   puts a.size
+# end
 
 h = a.map(&:to_json)
+
+puts h.inspect
+
+def to_j(s)
+  Oj.load(s)
+end
+
+h = a.map(&method(:to_j))
 
 puts h.inspect
