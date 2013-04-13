287c1a89139801789a28740d8d36a53a7ce20596
Wlodek Bzyl <matwb@univ.gda.pl>
2013-04-13 20:35:11 +0200

    poprawki w 4books.js

--- pp/mapreduce/mongo/4books.js
+++ pp/mapreduce/mongo/4books.js
@@ -1,13 +1,16 @@
 m = function() {
-  this.paragraph.toLowerCase().match(/[A-Za-z\u00C0-\u017F]+/g).forEach(function(word) {
-    emit(word, 1);
-  });
+  res = this.p.toLowerCase().match(/[\w\u00C0-\u017F]+/g);
+  if (res) {
+    res.forEach(function(word) {
+      emit(word, 1);
+    });
+  }
 };
 
 r = function(key, values) {
   return Array.sum(values);
 };
 
-var res = db.chesterton.mapReduce(m, r, {out: "wc"});
+var res = db.books.mapReduce(m, r, {out: "wc"});
 
 printjson(res);
