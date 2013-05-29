f4ede7c5e67b9ed79a2f20e1b5ce5b28c469eec1
Wlodek Bzyl <matwb@univ.gda.pl>
2013-05-29 14:32:16 +0200

    fix: generator przemówień

--- pp/mapreduce/couch/polish/generate-paragraph.rb
+++ pp/mapreduce/couch/polish/generate-paragraph.rb
@@ -1,29 +1,36 @@
 #! /usr/bin/env ruby
 # -*- coding: utf-8 -*-
-
 require 'bundler/setup'
-
 require 'couchrest'
 
-couch = CouchRest.new("http://localhost:5984")
-DB = couch.database('wb')
-
-WORD_MEMOIZER = {}
-
-def probable_follower_for(start)
-  WORD_MEMOIZER[start] ||= DB.view('app/markov', startkey: start, :endkey=>[start, {}].flatten, :group_level=>3)
-  row = WORD_MEMOIZER[start]['rows'].sample # get random row (Ruby v1.9.2+)
-  return row['key'][1,2]
-end
-
-bigram = ["★", "★"]
-while true
-  bigram = probable_follower_for(bigram)
-  if bigram[1] == "◀"
-    break
-  else
-    print "#{bigram[1]} "
+class Generator
+  def initialize(couch, db)
+    couch = CouchRest.new("http://localhost:5984")
+    @db = couch.database('wb')
   end
+
+  def run
+    bigram = ["★", "★"]
+    while true
+      bigram = probable_follower_for(bigram)
+      if bigram[1] == "◀"
+        break
+      else
+        print "#{bigram[1]} "
+      end
+    end
+  end
+
+private
+
+  def probable_follower_for(start)
+    view = @db.view('app/markov', startkey: start, :endkey=>[start, {}].flatten, :group_level=>3)
+    row = view['rows'].sample # get random row (Ruby v1.9.2+)
+    return row['key'][1,2]
+  end
+
 end
 
+generator = Generator.new("http://localhost:5984", "wb")
+generator.run
 print "\n"
