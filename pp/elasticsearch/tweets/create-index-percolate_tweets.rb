5e9205511ba97ae42910902f055cfd2a9867380c
Wlodek Bzyl <matwb@univ.gda.pl>
2013-11-14 17:15:55 +0100

    poprawki: create-index-percolate_tweets.rb

--- pp/elasticsearch/tweets/create-index-percolate_tweets.rb
+++ pp/elasticsearch/tweets/create-index-percolate_tweets.rb
@@ -5,20 +5,24 @@ require "bundler/setup"
 require "elasticsearch"
 require "colored"
 
-mapping =  {
-  :properties => {
-    :text          => { :type    => 'string', :boost => 2.0,            :analyzer => 'snowball'       },
-    :screen_name   => { :type    => 'string', :index => 'not_analyzed'                                },
-    :created_at    => { :type    => 'date'                                                            },
-    :hashtags      => { :type    => 'string', :index => 'not_analyzed', :index_name => 'hashtag'      },
-    :urls          => { :type    => 'string', :index => 'not_analyzed', :index_name => 'url'          },
-    :user_mentions => { :type    => 'string', :index => 'not_analyzed', :index_name => 'user_mention' },
-    :_ttl          => { :enabled => true,     :default => "30d"                                       }
+# http://rubydoc.info/gems/elasticsearch-api
+# client.indices.put_mapping index: "tweets", type: "rails",
+#   body: { rails: { properites: { created_at: { type: "date", format: "YYYY-MM-dd HH:mm:ss Z" } } } }
+
+mapping = {
+  properties: {
+    created_at:    { type: 'date',   format:  'YYYY-MM-dd HH:mm:ss Z', store: true           },
+    text:          { type: 'string', boost:    2.0,                    analyzer:  'snowball' },
+    screen_name:   { type: 'string', index:   'not_analyzed'                                 },
+    hashtags:      { type: 'string', index:   'not_analyzed'                                 },
+    urls:          { type: 'string', index:   'not_analyzed'                                 },
+    user_mentions: { type: 'string', index:   'not_analyzed'                                 }
+#   _ttl:          { enabled: true,  default: '12w'                                          }
   }
 }
 
 topics = %w[
-  mongodb elasticsearch couchdb neo4j redis emberjs meteorjs rails d3js
+  deeplearning mongodb elasticsearch couchdb neo4j redis emberjs meteorjs rails d3js
 ]
 
 # http://rubydoc.info/gems/elasticsearch-transport
@@ -27,26 +31,13 @@ elasticsearch_client = Elasticsearch::Client.new log: true
 
 # elasticsearch_client.perform_request :delete, '/tweets'
 
-#      perform_request method,  path,  params = {}, body = nil
-elasticsearch_client.perform_request :put,   '/tweets'  # create ‘tweets’ index
-
-# http://rubydoc.info/gems/elasticsearch-api
-
-# elasticsearch_client.indices.put_mapping index: 'tweets', type: 'mongodb', body: {
-#   mongodb: {
-#     properties: {
-#       text: { type: 'string', boost: 2.0, analyzer: 'snowball' },
-#       screen_name: { type: 'string', index: 'not_analyzed' },
-#       created_at: { type: 'date' },
-#       hashtags: { type: 'string', index: 'not_analyzed', index_name: 'hashtag' },
-#       urls: { type: 'string', index: 'not_analyzed', index_name: 'url' },
-#       user_mentions: { type: 'string', index: 'not_analyzed', index_name: 'user_mention' }
-#     }
-#   }
-# }
+elasticsearch_client.perform_request :put, '/tweets'  # create ‘tweets’ index
 
 topics.each do |keyword|
-  elasticsearch_client.indices.put_mapping index: 'tweets', type: keyword, body: mapping
+  keyword_mapping = { }
+  keyword_mapping[keyword.to_sym] = mapping
+  elasticsearch_client.indices.put_mapping index: 'tweets', type: keyword,
+    body: keyword_mapping
 end
 
 # elasticsearch_client.indices.get_mapping index: 'tweets', type: 'mongodb'
