ed9374ada91379e52c66d1cdfb2230e384a650ef
Włodek Bzyl <wbzyl@mbook.local>
2015-10-29 22:49:26 +0100

    update dostojewski.rb to v2.1 mongo driver

--- pp/db/mongodb/dostojewski.rb
+++ pp/db/mongodb/dostojewski.rb
@@ -6,11 +6,12 @@ require 'net/http'
 
 # http://www.ruby-doc.org/ruby-1.9/classes/Logger.html
 require 'logger'
-logger = Logger.new(STDERR)
-logger.level = Logger::INFO  # set the default level: INFO, WARN
 
 require 'mongo'
 include Mongo
+
+logger = Logger.new($stderr)
+logger.level = Logger::WARN # set the default level: INFO, WARN
 
 # English stopwords from Tracker, http://projects.gnome.org/tracker/
 # GitHub: git clone git://git.gnome.org/tracker ; cd data/languages/
@@ -21,13 +22,9 @@ logger.info "liczba wczytanych stopwords: #{stop.length}"
 uri = 'http://www.gutenberg.org/cache/epub/2638/pg2638.txt'
 filename = 'Dostoevsky_Feodor-The_Idiot.txt'
 
-dbname = 'test'
-collection = 'dostojewski'
-
-
 # Gutenberg
 
-unless File.exists?(filename)
+unless File.exist?(filename)
   url = URI.parse(uri)
   req = Net::HTTP::Get.new(url.path)
   logger.info "connecting to #{url.host}"
@@ -41,63 +38,57 @@ unless File.exists?(filename)
   end
 end
 
-# read the file content in the „DOS” paragraph mode and remove empty paragraphs
+# read the file content in the DOS paragraph mode and remove empty paragraphs
 
-# data[4065]
-# => "\nGania began, but did not finish. The two--father and son--stood before\r\none another, both unspeakably agitated, especially Gania.\r\n\r"
-# data[4065].gsub!(/\s+/, " ")
-# => " Gania began, but did not finish. The two--father and son--stood before one another, both unspeakably agitated, especially Gania. "
-
-data = IO.readlines(filename, "\n\r") # read DOS file; UNIX .readlines(filename, "")
+# read DOS file; UNIX .readlines(filename, "")
+data = IO.readlines(filename, "\n\r")
 lines = data.map do |para|
-  para.gsub!(/\s+/, " ").strip
+  para.gsub!(/\s+/, ' ').strip
 end
 
 # delete empty strings and strip legal info (preamble and postable)
 
-lines.delete("")
+lines[1..4]
+
+lines.delete('')
 book = lines[12..-56]
+
 logger.info "liczba wczytanych akapitów: #{book.size}"
 
-# updated to MongoDB Driver 1.9.2
+# updated to MongoDB Driver 2.1.2
 
-# connection = Mongo::Connection.new("localhost", 27017)
-# connection.drop_database(dbname)
+Mongo::Logger.logger = logger
+Mongo::Logger.logger.level = Logger::WARN
 
-connection = MongoClient.new("localhost", 27017)
-# connection.drop_database(dbname)
+client = Mongo::Client.new('mongodb://127.0.0.1:27017/test')
+coll = client[:dostojewski]
 
-db = connection.db(dbname)
-# db.authenticate("username", "password") # authenticate
-
-db.drop_collection(collection)
-
-coll = db.collection(collection)
+coll.drop
 
 book.each_with_index do |para, n|
   words = para
-    .gsub(/[!?;:"'().,\[\]*]/, "")
-    .gsub("--", " ")
-    .gsub("_", " ")
-    .gsub("\ufeff", "") # remove zero width no-break space in the title
-    .downcase.split(/\s+/)
+          .gsub(/[!?;:"'().,\[\]*]/, '')
+          .gsub('--', ' ')
+          .tr('_', ' ')
+          .gsub('\ufeff', '') # remove zero width no-break space in the title
+          .downcase.split(/\s+/)
   words.each do |word|
     if stop.include?(word)
       logger.debug "skipped stopword: #{word}"
     else
       next if word.empty?
-      letters = word.split("").sort.uniq
-      coll.insert({
+      letters = word.split('').sort.uniq
+      coll.insert_one(
         word: word,
         para: n,
         letters: letters
-      })
+      )
       logger.debug "word: #{word}, para: #{n}, letters: #{letters}\n"
     end
   end
 end
 
-logger.info "MongoDB:"
-logger.info "\t  database: #{dbname}"
-logger.info "\tcollection: #{collection}"
-logger.info "\t     count: #{coll.count}"
+puts 'Done!'
+puts "\t  database: test"
+puts "\tcollection: dostojewski"
+puts "\t      size: #{coll.find.count}"
