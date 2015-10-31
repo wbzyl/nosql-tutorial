d8e317ba1323821e5892577abe209c5a5f2a7c5d
Włodek Bzyl <wbzyl@mbook.local>
2015-10-31 10:47:06 +0100

    add hello-logger.rb

--- pp/db/mongodb/hello-logger.rb
+++ pp/db/mongodb/hello-logger.rb
@@ -6,22 +6,24 @@ require('logger')
 # logger levels
 # DEBUG < INFO < WARN < ERROR < FATAL < UNKNOWN
 
+levels = {
+  debug: Logger::DEBUG,
+  info: Logger::INFO,
+  warn: Logger::WARN,
+  error: Logger::ERROR,
+  fatal: Logger::FATAL,
+  unknown: Logger::UNKNOWN
+}
+
 logger = Logger.new(STDOUT)
-logger.level = Logger::WARN
 
-logger.debug 'Created logger'
-logger.info 'Program started'
-logger.warn 'Nothing to do!'
+# set default level to Logger::INFO
+level = levels[ARGV[0].to_s.downcase.to_sym] || Logger::INFO
+logger.level = level
 
-path = 'a_non_existent_file'
-
-begin
-  File.foreach(path) do |line|
-    unless line =~ /^(\w+) = (.*)$/
-      logger.error "Line in wrong format: #{line.chomp}"
-    end
-  end
-rescue => err
-  logger.fatal 'Caught exception; exiting'
-  logger.fatal err
-end
+logger.debug 'Debug: created logger (low-level info for developers)'
+logger.info 'Info: program started'
+logger.warn 'Warn: nothing to do!'
+logger.error 'Error: a handleable condition occured!'
+logger.fatal 'Fatal: unhandleable error that results in a program crash.'
+logger.unknown 'Unknown: message that should always be logged.'
