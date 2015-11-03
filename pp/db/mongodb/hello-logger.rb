26c14ebc4b4383427d59348c5b71e7d7d2d91eb2
Włodek Bzyl <wbzyl@mbook.local>
2015-11-03 14:22:48 +0100

    add .rubocop.yml

--- pp/db/mongodb/hello-logger.rb
+++ pp/db/mongodb/hello-logger.rb
@@ -1,5 +1,7 @@
 #! /usr/bin/env ruby -w
 
+# rubocop:disable Metrics/LineLength
+
 # https://rubymonk.com/learning/books/4-ruby-primer-ascent/chapters/50-debugging/lessons/123-ruby_logging
 require('logger')
 
