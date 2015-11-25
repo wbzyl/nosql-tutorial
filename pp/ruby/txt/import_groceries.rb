d96548a5516904a5f016978d694c9e1bb300db63
Włodek Bzyl <wbzyl@mbook.local>
2015-11-25 19:42:48 +0100

    add example: ruby+mongo+groceries transactions

---
+++ pp/ruby/txt/import_groceries.rb
@@ -0,0 +1,18 @@
+#! /usr/bin/env ruby
+
+# Usage:
+#   bunzip2 -c groceries.txt.bz2 | ./import_groceries.rb
+
+require 'bundler/setup'
+require 'mongo'
+
+Mongo::Logger.level = Logger::INFO
+
+client = Mongo::Client.new('mongodb://localhost:27017/test')
+coll = client[:groceries]
+coll.drop
+
+ARGF.each_line do |line|
+  puts "transaction: #{ARGF.lineno}"
+  coll.insert_one(_id: ARGF.lineno, t: line.rstrip.split(','))
+end
