c1f4b151ae448926c1aaaf4346a4b68357777fc9
Wlodek Bzyl <matwb@univ.gda.pl>
2017-03-03 12:32:16 +0100

    update

--- pp/elasticsearch/tweets/fetch-tweets.rb
+++ pp/elasticsearch/tweets/fetch-tweets.rb
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
-require "bundler/setup"
+require 'bundler/setup'
 
 # https://github.com/sferik/twitter,
 # http://rdoc.info/github/sferik/twitter
-require 'twitter'  # we need at least this version 5.0.0.rc1
-
+require 'twitter'
 require 'colored'
 require 'elasticsearch'
 
@@ -18,21 +16,21 @@ class Status
     @status = cleanup(status)
   end
 
-private
+  private
 
   # strip off fields we are not interested in
   # flatten and clean up entities
-  def cleanup(s)
-    # use these fields:
-    #   :created_at, :text, :screen_name,
-    #   :hashtags, :urls, :user_mentions
+  # we use these fields:
+  #   :created_at, :text, :screen_name,
+  #   :hashtags, :urls, :user_mentions
+  # puts "#{s.methods(all: false)}".red
 
-    # puts "#{s.methods(all: false)}".red
+  def cleanup(s)
     puts "language: #{s.lang}".red
-    puts "#hashtags: #{s.hashtags.size}".bold if s.hashtags.any? 
-    hashtags = s.hashtags.to_a.map { |o| o["text"] }
-    urls = s.urls.to_a.map { |o| o["expanded_url"] }
-    user_mentions = s.user_mentions.to_a.map { |o| o["screen_name"] }
+    puts "#hashtags: #{s.hashtags.size}".bold if s.hashtags.any?
+    hashtags = s.hashtags.to_a.map { |o| o['text'] }
+    urls = s.urls.to_a.map { |o| o['expanded_url'] }
+    user_mentions = s.user_mentions.to_a.map { |o| o['screen_name'] }
     {
       text: s[:text],
       screen_name: s[:user][:screen_name],
@@ -57,42 +55,45 @@ end
 
 begin
   raw_config = File.read File.expand_path(credentials[0])
-  twitter = YAML.load(raw_config)
+  twitter = YAML.safe_load(raw_config)
 rescue
   puts "\n\tError: problems with #{credentials}\n".red
   exit(1)
 end
 
-# https://dev.twitter.com/apps
-#   My applications: Elasticsearch NoSQL
-
 twitter_client = Twitter::Streaming::Client.new do |config|
   config.consumer_key        = twitter['consumer_key']
   config.consumer_secret     = twitter['consumer_secret']
-  config.access_token        = twitter['oauth_token']
-  config.access_token_secret = twitter['oauth_token_secret']
+  config.access_token        = twitter['access_token']
+  config.access_token_secret = twitter['access_token_secret']
 end
+
+topics = %w(
+  deeplearning
+  mongodb elasticsearch neo4j redis
+  rails
+)
+
+twitter_client.filter(track: topics.join(',')) do |status|
+  tweet = Status.new(status)
+  puts tweet.inspect
+end
+
+__END__
 
 elasticsearch_client = Elasticsearch::Client.new
 
-topics = %w[
-  deeplearning
-  mongodb elasticsearch couchdb neo4j redis
-  emberjs meteorjs rails
-  d3js
-]
-
-twitter_client.filter(track: topics.join(",")) do |status|
+twitter_client.filter(track: topics.join(',')) do |status|
   tweet = Status.new(status)
-  result = elasticsearch_client.percolate index: 'tweets', body: { doc: tweet.status }
+  # result = elasticsearch_client.percolate index: 'tweets', body: { doc: tweet.status }
 
-  puts "#{tweet.status[:created_at].to_s.cyan}:  #{tweet.status[:text].yellow}"
-  puts "percolated query: #{result["matches"]}".green
+  # puts "#{tweet.status[:created_at].to_s.cyan}:  #{tweet.status[:text].yellow}"
+  # puts "percolated query: #{result["matches"]}".green
 
-  result["matches"].each do |type|
-    elasticsearch_client.index index: "tweets", type: type, id: tweet.id,
-      body: tweet.status
-  end
+#  result['matches'].each do |type|
+#    elasticsearch_client.index index: "tweets", type: type, id: tweet.id,
+#      body: tweet.status
+#  end
 end
 
 # You can check out the statuses in your index with curl
